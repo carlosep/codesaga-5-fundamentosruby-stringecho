7aed3d8eb58ee5b742f27c2dd7ad471c35f49f22
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-27 10:36:56 -0200

    Strings - Concluído

--- lib/echo.rb
+++ lib/echo.rb
@@ -1,2 +1,13 @@
 class Echo
+	def message(message)
+		if message.strip.empty?
+			@message = "Nada?"
+		elsif message.include? "!!!"
+			@message = "Não faço echo de gritos."
+		elsif message[-1].include? "?"
+			message = "Só faço echo, não respondo perguntas."
+		else
+			@message = message
+		end
+	end
 end
